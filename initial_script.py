d0eff1facbef51b0cef03e3aa47af6925b762340
mallku <johnybray@hotmail.com>
2025-04-09 23:19:42 +0100

    Initial script: set up of initial analysis file

---
+++ initial_script.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  9 23:11:35 2025
+
+@author: johny
+"""
+
+from pathlib import Path
+from textwrap import fill
+
+import numpy as np
+import pandas as pd
+import requests
+import seaborn as sns
+import tiktoken
+from sklearn.model_selection import train_test_split
+#from tqdm.auto import tqdm
